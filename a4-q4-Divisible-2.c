f0ef0a6a279e0c298f867be659f1292a2ae2ba7c
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2025-08-02 19:25:49 +0600

    code

--- a4-q4-Divisible-2.c
+++ a4-q4-Divisible-2.c
@@ -51,7 +51,7 @@ int main()
     scanf("%d", &N);
     for (int i = 1; i <= N; i++)
     {
-        if (i % 3 == 0 & i % 7 == 0)
+        if (i % 3 == 0 && i % 7 == 0)
         {
             printf("%d\n", i);
         }
